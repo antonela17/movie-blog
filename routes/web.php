9524bec641016618dfed0c9e01ae99ba47d412da
antonela17 <antonelakolaj17@gmail.com>
2022-09-18 16:36:42 +0200

    Middleware fixes

--- routes/web.php
+++ routes/web.php
@@ -16,61 +16,64 @@ use App\Mail\ContactEmail;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/json',[\App\Http\Controllers\readDataFromJson::class,'test']);
-
 
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::group(['middleware' => 'user_only'], function () {
 
-Route::get('/home',[\App\Http\Controllers\HomePageController::class,'show']);
+    Route::get('/home', [\App\Http\Controllers\HomePageController::class, 'show']);
 
-Route::get('/home/contact', [\App\Http\Controllers\ContactController::class,'create']);
-Route::post('/home/contact',[\App\Http\Controllers\ContactController::class, 'send'])->name('contact.send');
+    Route::get('/home/contact', [\App\Http\Controllers\ContactController::class, 'create']);
+    Route::post('/home/contact', [\App\Http\Controllers\ContactController::class, 'send'])->name('contact.send');
 
-Route::get('/home/about', function () {
-    return view('about');
-});
+    Route::get('/home/about', function () {
+        return view('about');
+    });
 
-Route::get('/home/project-details', function () {
-    return view('project-details');
-});
+    Route::get('/home/project-details', function () {
+        return view('project-details');
+    });
 
-Route::get('/home/projects', function () {
-    return view('projects');
-});
+    Route::get('/home/projects', function () {
+        return view('projects');
+    });
 
-Route::get('/home/sample-inner-page', function () {
-    return view('sample-inner-page');
-});
+    Route::get('/home/sample-inner-page', function () {
+        return view('sample-inner-page');
+    });
 
-Route::get('/home/service-details', function () {
-    return view('service-details');
-});
+    Route::get('/home/service-details', function () {
+        return view('service-details');
+    });
 
-Route::get('/home/services', function () {
-    return view('services');
-});
+    Route::get('/home/services', function () {
+        return view('services');
+    });
 
 //Route::get('/home/email', function () {
 //  Mail::to('info@gmail.com')->send(new ContactEmail());
 //    return new ContactEmail();
 //});
 
-Route::get('/home/movies',[\App\Http\Controllers\MoviesController::class,'show']);
-Route::post('/home/movies',[\App\Http\Controllers\SearchController::class,'search'])->name('search');
+    Route::get('/home/movies', [\App\Http\Controllers\MoviesController::class, 'show']);
+    Route::post('/home/movies', [\App\Http\Controllers\SearchController::class, 'search'])->name('search');
 
-Route::get('/home/movies/create',[\App\Http\Controllers\MoviesController::class,"showCreateMovie"])->name('movie.showCreate');
-Route::post("/home/movies/create",[\App\Http\Controllers\MoviesController::class,'createMovie'])->name('movie.create');
+    Route::group(['middleware' => 'admin_only'], function () {
+        Route::get('/home/movies/create', [\App\Http\Controllers\MoviesController::class, "showCreateMovie"])->name('movie.showCreate');
+        Route::post("/home/movies/create", [\App\Http\Controllers\MoviesController::class, 'createMovie'])->name('movie.create');
+        Route::post('/home/delete/{id}', [\App\Http\Controllers\MoviesController::class, 'destroy'])->name('movie.delete');
+        Route::get('/home/movies/edit/{id}', [\App\Http\Controllers\MoviesController::class, "showEditPage"])->name('movie.showEdit');
+        Route::post('/home/movies/edit/{id}', [\App\Http\Controllers\MoviesController::class, 'edit'])->name('movie.edit');
+    });
 
-Route::get('/home/movies/{slug}', [\App\Http\Controllers\CategoryController::class,'show']);
+    Route::get('/home/movies/{slug}', [\App\Http\Controllers\CategoryController::class, 'show']);
 
-Route::get('/home/{slug}',[\App\Http\Controllers\MovieDetailsController::class,'show']);
-Route::post('/home/delete/{id}',[\App\Http\Controllers\MoviesController::class,'destroy'])->name('movie.delete');
+    Route::get('/home/{slug}', [\App\Http\Controllers\MovieDetailsController::class, 'show']);
 
-Route::get('/home/movies/edit/{id}',[\App\Http\Controllers\MoviesController::class,"showEditPage"])->name('movie.showEdit');
-Route::post('/home/movies/edit/{id}',[\App\Http\Controllers\MoviesController::class,'edit'])->name('movie.edit');
+});
+
 
 //Route::get('/home/edit', function () {
 //    $categories = Categories::all()->toArray();
